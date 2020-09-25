ecb37425ff3453b0d0666c4c56be7b5d4c86aac4
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-24 19:48:19 -0700

    Add EnvironmentVariables with API key.

--- SaferSpaces/Controllers/PlacesController.cs
+++ SaferSpaces/Controllers/PlacesController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SaferSpacesClient.Models;
+
+
 namespace SaferSpacesClient.Controllers
 {
   public class PlacesController : Controller
